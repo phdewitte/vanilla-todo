e0c0b5f9e8a689a96eb36b744e08f74f88b39573
Patrick DeWitte <patrickhdewitte@gmail.com>
2018-12-26 16:57:55 -0600

    Add view function for checked toggle

--- src/Controller.ts
+++ src/Controller.ts
@@ -11,61 +11,62 @@ export default class Controller {
     this.store = store;
     this.view = view;
 
-    this.addItemToList = this.addItemToList.bind(this);
+    this.addTodo = this.addTodo.bind(this);
     this.handleListItemClick = this.handleListItemClick.bind(this);
 
-    this.attachAddTodoListener();
-    this.attachItemClickListener();
+    this.attachTodoFormSubmitListener();
+    this.attachListItemClickListener();
   }
 
-  public attachAddTodoListener() {
-    const form = document.getElementById('todo-form');
-    form!.addEventListener('submit', this.addItemToList);
+  public attachTodoFormSubmitListener() {
+    const form = this.view.getElement('todo-form');
+    form!.addEventListener('submit', this.addTodo);
   }
 
-  public attachItemClickListener() {
-    const list = document.getElementById('todo-list');
+  public attachListItemClickListener() {
+    const list = this.view.getElement('todo-list');
     list!.addEventListener('click', this.handleListItemClick);
   }
 
   public handleListItemClick(event: Event) {
+    // TODO: Better way to pass id w/ both input and button elements?
     const { id, nodeName } = event.target as HTMLElement;
-
     if (nodeName === 'INPUT') {
-      const item = this.store.todos[id];
-      const updatedItem = { ...item, checked: !item.checked };
-      this.editItem(updatedItem);
+      this.toggleTodo(id);
     } else if (nodeName === 'BUTTON') {
       const { value } = event.target as HTMLButtonElement;
-      const item = this.store.todos[value];
-      this.deleteItem(item);
+      this.deleteTodo(value);
     }
   }
 
-  public createItem(): Item {
-    const inputElement = document.getElementById('todo-input') as HTMLInputElement;
+  public createTodoItem(): Item {
+    const inputElement = this.view.getElement('todo-input') as HTMLInputElement;
     const text = inputElement.value;
-
     inputElement.value = '';
-
     return { text, id: String(Date.now()), checked: false };
   }
 
-  public addItemToList(event: Event): void {
+  public addTodo(event: Event) {
     event.preventDefault();
-
-    const newItem = this.createItem();
+    const newItem = this.createTodoItem();
     this.store.upsertItem(newItem);
-    this.view.appendItem(newItem);
+    this.view.appendListItem(newItem);
   }
 
-  public editItem(item: Item): void {
+  public toggleTodo(id: string) {
+    const item = this.store.todos[id];
+    const updatedItem = { ...item, checked: !item.checked };
+    this.editTodo(updatedItem);
+  }
+
+  public editTodo(item: Item) {
     this.store.upsertItem(item);
-    // TODO: View is currently 'uncontrolled'. Add view update
+    this.view.editListItem(item);
   }
 
-  public deleteItem(item: Item): void {
-    this.store.deleteItemById(item.id);
-    this.view.removeItem(item);
+  public deleteTodo(id: string) {
+    const item = this.store.todos[id];
+    this.store.deleteItemById(id);
+    this.view.removeListItem(item);
   }
 }
--- src/View.ts
+++ src/View.ts
@@ -11,29 +11,41 @@ export default class View {
     return `li-${id}`;
   }
 
-  public createListEntry(item: Item) {
-    const li = document.createElement('li');
-    li.setAttribute('id', this.getIdForListItem(item.id));
-
+  public getListItemChildMarkup(item: Item) {
     const checkedString = item.checked ? 'checked' : '';
-
     // Research wrapped label-wrapped inputs and accessibility benefits
-    const markup =
+    return (
       `<input id=${item.id} type="checkbox" ${checkedString}>
       <label for=${item.id}>${item.text}</label>
-      <button type="button" value=${item.id}>Delete Todo</button>`;
+      <button type="button" value=${item.id}>Delete Todo</button>`
+    );
+  }
 
-    li.innerHTML = markup;
+  public getElement(id: string) {
+    return document.getElementById(id);
+  }
 
+  public createListItem(item: Item) {
+    const li = document.createElement('li');
+    const id = this.getIdForListItem(item.id);
+    li.setAttribute('id', id);
+    li.innerHTML = this.getListItemChildMarkup(item);
     return li;
   }
 
-  public appendItem(item: Item) {
-    const li = this.createListEntry(item);
+  public appendListItem(item: Item) {
+    const li = this.createListItem(item);
     this.list!.appendChild(li);
   }
 
-  public removeItem(item: Item) {
+  public editListItem(item: Item) {
+    const idToUpdate = this.getIdForListItem(item.id);
+    const elementToUpdate = document.getElementById(idToUpdate) as HTMLElement;
+    const markup = this.getListItemChildMarkup(item);
+    elementToUpdate.innerHTML = markup;
+  }
+
+  public removeListItem(item: Item) {
     const idToRemove = this.getIdForListItem(item.id);
     const elementToRemove = document.getElementById(idToRemove) as HTMLElement;
     elementToRemove!.parentNode!.removeChild(elementToRemove);
